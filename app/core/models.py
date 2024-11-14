33db7b3a6b0eb422f805fd36ca86c447c8917283
is6769 <igor.shvets2007@mail.ru>
2024-11-14 19:12:59 +0300

    created core, and made CustomUser model v1

---
+++ app/core/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    birth_date = models.DateField(null=True, blank=True)
+    surname = models.CharField(max_length=50, blank=True)
+    profile = models.CharField(max_length=50, blank=True)
+    course = models.IntegerField(null=True)
+    additional_info = models.TextField(null=True)
+    gpa = models.FloatField(null=True, blank=True)
+    perc = models.FloatField(null=True, blank=True)

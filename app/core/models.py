4654cb068cf34f112d004d97f25e37f2d8c633f5
is6769 <igor.shvets2007@mail.ru>
2024-11-15 10:12:59 +0300

    Add models related to schedule

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,Group
 from django.db import models
 
 
@@ -17,6 +17,37 @@ class CustomUser(AbstractUser):
         db_table = 'custom_user'
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
-        
+
+class Subject(models.Model):
+    name = models.CharField(max_length=50);
+
+class TypeOfLesson(models.Model):
+    name = models.CharField(max_length=50);
+
+class LessonLocation(models.Model):
+    name = models.CharField(max_length=50);
+
+'''
+Model describing lesson
+'''
+class Lesson(models.Model):
+    subjectId =models.ForeignKey(Subject, on_delete=models.CASCADE);
+    groupId = models.ManyToManyField(Group, related_name="groups", blank=True);
+    number = models.IntegerField(null=True);
+    startTime = models.DateTimeField();
+    endTime = models.DateTimeField();
+    type = models.ForeignKey(TypeOfLesson, on_delete=models.CASCADE);
+    location = models.ForeignKey(LessonLocation, on_delete=models.CASCADE);
+    date = models.DateField();
+
+
+
+class Schedule(models.Model):
+    lessonId = models.ForeignKey(Lesson, on_delete=models.CASCADE); # link to tutor's lessons
+    userId = models.ForeignKey(CustomUser, on_delete=models.CASCADE); #link to tutor
+
+
+
+
     
         

e488fa890b8b8923ac83057a1207df0a917e77c1
polargreyOff <124440447+polargreyOff@users.noreply.github.com>
2024-11-20 16:06:33 +0300

    Add files via upload

--- ww/src/main.js
+++ ww/src/main.js
@@ -1,7 +1,7 @@
 import './assets/nullstyle.css'
 import './assets/main.css'
 import axios from "axios";
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = 'http://localhost:8000/api/v1';
 
 
 import { createApp } from 'vue'
